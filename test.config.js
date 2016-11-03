4160660b44f3865b5018173e2d57c20678823aaa
dtothefp <dtothefp@gmail.com>
2016-11-03 12:39:42 -0400

    stuff

---
+++ test.config.js
@@ -0,0 +1,17 @@
+import 'babel-polyfill';
+/**
+ * Bootstrap file for Webpack pre-preprocessor utilized in Karma tests
+ */
+const {TEST_FILE} = process.env;
+const re = TEST_FILE ?   new RegExp(`${TEST_FILE}\.js`) : /-spec\.js$/;
+
+/**
+ * require all the test files unless a single file is specified
+ */
+const testCtx = require.context('./test', true, /-spec\.js$/);
+
+testCtx.keys().forEach((key) => {
+  if (re.test(key)) {
+    testCtx(key);
+  }
+});
---
+++ test/sample-spec.js
@@ -0,0 +1,8 @@
+import react from 'react';
+import enzyme from 'enzyme';
+
+describe('test', () => {
+  it('should work', () => {
+
+  });
+});
